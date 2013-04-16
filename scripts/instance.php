37f74974f16e52dc78dbd68bff837c3b348c2996
Paul M. Jones <pmjones88@gmail.com>
2013-04-15 21:20:41 -0500

    add PSL to the instance

--- scripts/instance.php
+++ scripts/instance.php
@@ -1,4 +1,9 @@
 <?php
 namespace Aura\Uri;
 require_once dirname(__DIR__) . '/src.php';
-return new Url\Factory($_SERVER);
+return new Url\Factory(
+    $_SERVER,
+    new PublicSuffixList(
+        include dirname(__DIR__) . '/data/public-suffix-list.php'
+    )
+);
