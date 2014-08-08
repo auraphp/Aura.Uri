642602a230ce258ba11a7c5837d0ac193039194a
Hari KT <kthari85@gmail.com>
2014-08-08 09:14:13 +0530

    branch alias

---
+++ config/Common.php
@@ -0,0 +1,28 @@
+<?php
+namespace Aura\Uri\_Config;
+
+use Aura\Di\Config;
+use Aura\Di\Container;
+
+class Common extends Config
+{
+    public function define(Container $di)
+    {
+        /**
+         * Aura\Uri\PublicSuffixList
+         */
+        $di->params['Aura\Uri\PublicSuffixList']['list'] = $di->lazyRequire(
+            dirname(__DIR__) . '/data/public-suffix-list.php'
+        );
+
+        /**
+         * Aura\Uri\Url\Factory
+         */
+        $di->params['Aura\Uri\Url\Factory']['server'] = $_SERVER;
+        $di->params['Aura\Uri\Url\Factory']['psl'] = $di->lazyNew('Aura\Uri\PublicSuffixList');
+    }
+
+    public function modify(Container $di)
+    {
+    }
+}
