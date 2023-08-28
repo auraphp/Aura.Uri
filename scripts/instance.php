7d0887c0a0e8fbfab920f62ad33de16beea1a119
Akihito Koriyama <akihito.koriyama@gmail.com>
2023-08-29 05:35:12 +0900

    Discard v1 package style
    Lost its role as a framework after v2

--- scripts/instance.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Aura\Uri;
-require_once dirname(__DIR__) . '/src.php';
-return new Url\Factory(
-    $_SERVER,
-    new PublicSuffixList(
-        include dirname(__DIR__) . '/data/public-suffix-list.php'
-    )
-);
